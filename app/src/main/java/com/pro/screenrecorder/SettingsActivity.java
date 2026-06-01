1f2e06c7db3d9a456b6d922b115012b9b43b4ccd
clockstrikegame <touillahcen4@gmail.com>
2026-06-01 17:58:57 +0200

    Create SettingsActivity.java

---
+++ app/src/main/java/com/pro/screenrecorder/SettingsActivity.java
@@ -0,0 +1,34 @@
+package com.pro.screenrecorder;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle(R.string.settings_title);
+        }
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings_container, new SettingsFragment())
+                .commit();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() { finish(); return true; }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preferences, rootKey);
+        }
+    }
+}
